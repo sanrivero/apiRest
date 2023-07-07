77141f7437d7c4a49d969a709e7f7bff102945dd
santiago <sanrivero@gmail.com>
2023-07-07 18:53:24 -0300

    solucion problema versiones

--- src/main/java/com/example/api/services/ClienteServiceImpl.java
+++ src/main/java/com/example/api/services/ClienteServiceImpl.java
@@ -3,9 +3,11 @@ package com.example.api.services;
 import com.example.api.entities.Cliente;
 import com.example.api.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ClienteServiceImpl implements IClienteService{
 
     @Autowired
